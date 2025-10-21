2e7526f3b4bf3bd134ea0168e41dfe953e2a707b
DESKTOP-T69JMMQ\신헌우 <shinhunwoo1122@gmail.com>
2025-10-21 09:28:49 +0900

    git테스트용 develop

--- src/main/java/com/example/git_flow/controller/MainController.java
+++ src/main/java/com/example/git_flow/controller/MainController.java
@@ -1,4 +1,13 @@
 package com.example.git_flow.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
 }
